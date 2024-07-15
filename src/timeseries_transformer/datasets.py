912182b1cfd3873e7cf2bd4832c719717e73285a
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-15 23:00:09 +0200

    feat: updated dataset logic

--- src/timeseries_transformer/datasets.py
+++ src/timeseries_transformer/datasets.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.model_selection import StratifiedKFold
 
 class FordDataset(Dataset):
+    """Dataset class for FordA dataset. The dataset is available at: https://archive.ics.uci.edu/ml/datasets/FordA"""
     def __init__(self, sequences, labels):
         self.labels = labels
         self.sequences = sequences
@@ -19,28 +20,8 @@ class FordDataset(Dataset):
         return sequence, label
 
 
-class FordDataset(Dataset):
-    def __init__(self, split="train"):
-        self.root_url = "https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/"
-        self.data = torch.tensor(np.loadtxt(self.root_url + "FordA_TRAIN.tsv", delimiter="\t"),
-                                 dtype=torch.float32) if split == "train" else torch.tensor(
-            np.loadtxt(self.root_url + "FordA_TEST.tsv", delimiter="\t"), dtype=torch.float32)
-        self.labels = self.data[:, 0]  # get first element from each example
-        self.sequences = self.data[:, 1:]  # get all elements after first element
-        self.labels[self.labels == -1] = 0  # change all -1 labels to 0
-        self.num_classes = len(torch.unique(self.labels))  # count the number of unique labels
-
-    def __len__(self):
-        return self.data.shape[0]
-
-    def __getitem__(self, idx):
-        sequence = torch.reshape(self.sequences[idx], (-1, 1))  # dim: seq_len x num_features
-        label = torch.reshape(self.labels[idx], (-1,))  # dim: 1 x 1
-
-        return sequence, label
-
-
-class DatasetManager:
+class DatasetBuilder:
+    """DatasetBuilder for FordA dataset. The dataset is available at: https://archive.ics.uci.edu/ml/datasets/FordA"""
     def __init__(self, split: str = "train", use_k_fold: bool = False, num_folds: None | int = None) -> None:
         self.root_url = "https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/"
         self.split = split
@@ -58,14 +39,15 @@ class DatasetManager:
         self.labels = self.raw_data[:, 0]  # get first element from each example
         self.sequences = self.raw_data[:, 1:]  # get all elements after first element
         self.labels[self.labels == -1] = 0  # change all -1 labels to 0
-        self.num_classes = len(torch.unique(self.labels))  # count the number of unique labels
 
-
-    def get_dataset(self) -> Dataset | list[Dataset]:
+    def get_dataset(self) -> FordDataset | dict[str, list[FordDataset]]:
+        """Return FordDataset object or a dictionary of k training/validation FordDataset objects if k-fold is used."""
         if self.use_k_fold:
+            train_datasets = []
+            val_datasets = []
             for train_idx, val_idx in self.skf.split(self.sequences, self.labels):
                 # Create PyTorch DataLoader for training and validation
-                train_sets.append(FordDatasetKfold(x[train_idx], y[train_idx]))
-                val_sets.append(FordDatasetKfold(x[val_idx], y[val_idx]))
-            return []
+                train_datasets.append(FordDataset(self.sequences[train_idx], self.labels[train_idx]))
+                val_datasets.append(FordDataset(self.sequences[val_idx], self.labels[val_idx]))
+            return {"train": train_datasets, "val": val_datasets}
         return FordDataset(self.sequences, self.labels)
