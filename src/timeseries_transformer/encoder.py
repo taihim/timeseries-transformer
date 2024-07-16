60821f737d8b889d62147d112da926868c74dc06
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-16 23:03:19 +0200

    refactor: fixed imports for encoder module

--- src/timeseries_transformer/encoder.py
+++ src/timeseries_transformer/encoder.py
@@ -1,22 +1,27 @@
-class PytorchEncoder(nn.Module):
-  def __init__(self, inputs, embed_size, num_heads, ff_dim, dropout=0):
-    super(PytorchEncoder, self).__init__()
-    # attention
-    self.embedding = nn.Linear(in_features=inputs.shape[-1], out_features=embed_size)
-    self.attention = PtMultiheadAttention(embed_size, num_heads, dropout=0.0)
-    self.linear1 = nn.Linear(embed_size, 1)
-    self.dropout1 = nn.Dropout(dropout)
-    self.layer_norm1 = nn.LayerNorm(normalized_shape=inputs.shape[-1], eps=1e-6)
+"""Encoder module for the timeseries transformer model."""
+from torch import nn
+from src.timeseries_transformer.attention import MultiHeadAttention
 
-    # feedforward
-    self.conv1 = nn.Conv1d(in_channels=inputs.shape[-1], out_channels=ff_dim, kernel_size=1)
-    self.relu1 = nn.ReLU()
-    self.dropout2 = nn.Dropout(dropout)
-    self.conv2 = nn.Conv1d(in_channels=ff_dim, out_channels=inputs.shape[-1], kernel_size=1)
-    self.layer_norm2 = nn.LayerNorm(normalized_shape=inputs.shape[1], eps=1e-6)
+class PytorchEncoder(nn.Module):
+    def __init__(self, inputs, embed_size, num_heads, ff_dim, dropout=0):
+        super(PytorchEncoder, self).__init__()
+        # attention
+        self.embedding = nn.Linear(in_features=inputs.shape[-1], out_features=embed_size)
+        self.attention = MultiHeadAttention(embed_size, num_heads, dropout=0.0)
+        self.linear1 = nn.Linear(embed_size, 1)
+        self.dropout1 = nn.Dropout(dropout)
+        self.layer_norm1 = nn.LayerNorm(normalized_shape=inputs.shape[-1], eps=1e-6)
+
+        # feedforward
+        self.conv1 = nn.Conv1d(in_channels=inputs.shape[-1], out_channels=ff_dim, kernel_size=1)
+        self.relu1 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+        self.conv2 = nn.Conv1d(in_channels=ff_dim, out_channels=inputs.shape[-1], kernel_size=1)
+        self.layer_norm2 = nn.LayerNorm(normalized_shape=inputs.shape[1], eps=1e-6)
 
 
   def forward(self, src):
+    """Perform forward pass of the encoder module."""
     x = self.embedding(src)
     x = self.attention(x, x, x)[0]
     x = self.linear1(x)
