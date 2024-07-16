6b4b9bb237f827943f20e8df05617a9304a36bb4
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-16 23:11:42 +0200

    refactor: clean up training code

--- src/timeseries_transformer/test.py
+++ src/timeseries_transformer/test.py
@@ -4,3 +4,19 @@ import torch
 print(torch.cuda.is_available())
 print(torch.cuda.current_device())
 print(torch.cuda.get_device_name())
+
+
+acc = 0
+iteration = 0
+for data in test_dataloader:
+  iteration += 1
+  inputs, labels = data
+  if torch.cuda.is_available():
+    inputs = inputs.cuda()
+    labels = labels.cuda()
+  outputs = models[1]["model"](inputs)
+  predictions = torch.argmax(outputs, axis=1)
+  correct_labels = labels.squeeze().int()
+
+  acc += (predictions == correct_labels).int().sum()/len(labels) * 100
+print(acc/iteration)
--- src/timeseries_transformer/train.py
+++ src/timeseries_transformer/train.py
@@ -9,7 +9,9 @@ eval_iters = 200
 
 cuda0 = torch.device('cuda:0')
 
-def train_one_epoch(epoch_index, train_data, val_data, model):
+
+def train_one_epoch(model, epoch_index, train_data, val_data):
+    """Train the model for one epoch."""
     train_running_loss = 0.
     train_last_loss = 0.
     train_correct = 0
@@ -59,41 +61,6 @@ def train_one_epoch(epoch_index, train_data, val_data, model):
 
     return train_last_loss, train_acc, val_last_loss, val_acc
 
-
-epochs = 5
-models = []
-losses = []
-accuracies = []
-for fold in range(len(train_dataloaders)):
-    print('FOLD {}:'.format(fold + 1))
-
-    train_dataloader = train_dataloaders[fold]
-    val_dataloader = val_dataloaders[fold]
-
-    model = EncoderClassifier(inputs=xp, embed_size=embed_size, num_heads=num_heads, ff_dim=ff_dim, dropout=dropout,
-                              num_blocks=num_transformer_blocks)
-    model.cuda()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    models.append({"model": model, "loss_fn": criterion, "opt": optimizer})
-
-    losses.append([])
-    accuracies.append([])
-
-    for epoch in range(epochs):
-        print('EPOCH {}:'.format(epoch + 1))
-
-        models[fold]["model"].train(True)
-        train_avg_loss, train_acc, val_avg_loss, val_acc = train_one_epoch(epoch, train_dataloader, val_dataloader,
-                                                                           models[fold])
-
-        print(train_avg_loss)
-        print(train_acc)
-        print(val_avg_loss)
-        print(val_acc)
-
-        losses[fold].append((train_avg_loss, val_avg_loss))
-        accuracies[fold].append((train_acc, val_acc))
 
 if __name__ == "__main__":
     models = []
