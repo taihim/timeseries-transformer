5a4a16f68179a9bc46d35bf59e60b23f804ca159
Taimur Ibrahim <tai.him18@gmail.com>
2024-07-16 23:04:28 +0200

    refactor: fixed formatting of encoder

--- src/timeseries_transformer/encoder.py
+++ src/timeseries_transformer/encoder.py
@@ -19,23 +19,22 @@ class PytorchEncoder(nn.Module):
         self.conv2 = nn.Conv1d(in_channels=ff_dim, out_channels=inputs.shape[-1], kernel_size=1)
         self.layer_norm2 = nn.LayerNorm(normalized_shape=inputs.shape[1], eps=1e-6)
 
+    def forward(self, src):
+        """Perform forward pass of the encoder module."""
+        x = self.embedding(src)
+        x = self.attention(x, x, x)[0]
+        x = self.linear1(x)
+        x = self.dropout1(x)
+        x = self.layer_norm1(x)
 
-  def forward(self, src):
-    """Perform forward pass of the encoder module."""
-    x = self.embedding(src)
-    x = self.attention(x, x, x)[0]
-    x = self.linear1(x)
-    x = self.dropout1(x)
-    x = self.layer_norm1(x)
+        res = x + src
+        res = res.reshape(res.shape[0], res.shape[2], res.shape[1])
 
-    res = x + src
-    res = res.reshape(res.shape[0], res.shape[2], res.shape[1])
+        x = self.conv1(res)
+        x = self.relu1(x)
+        x = self.dropout2(x)
+        x = self.conv2(x)
+        x = self.layer_norm2(x)
+        x = x + res
 
-    x = self.conv1(res)
-    x = self.relu1(x)
-    x = self.dropout2(x)
-    x = self.conv2(x)
-    x = self.layer_norm2(x)
-    x = x + res
-
-    return x.reshape(x.shape[0], x.shape[-1], x.shape[1])
+        return x.reshape(x.shape[0], x.shape[-1], x.shape[1])
